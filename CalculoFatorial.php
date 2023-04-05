704ba4d08e3988eda03822b57d607dc1c66e12a1
EduuSouzaf <eduardofarias@alunos.utfpr.edu.br>
2023-04-05 14:00:18 -0300

    Calculo Fatorial

---
+++ CalculoFatorial.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculo Fatorial</title>
+</head>
+<body>
+<form action = "fatorial.php" method = "post">
+
+    Digite um Numero: <label>
+        <input name="num" type="text">
+    </label><br>
+
+    <input type="submit" name="ler" value="enviar">
+
+</form>
+</body>
+</html>
